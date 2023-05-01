560ae4110ea60fa2b3963c41dc29245e343dbfca
chad hinds <chadrickj8@gmail.com>
2023-05-01 12:52:35 -0400

    Initial header setup

--- app/api/components/Header.tsx
+++ app/api/components/Header.tsx
@@ -1,6 +1,8 @@
 const Header = () => {
     return (
-        <>Header ting</>
+        <header className="bg-blue-600 py-4">
+            <h1 className="Header">Wiki Viewer</h1>
+        </header>
     );
 };
 
