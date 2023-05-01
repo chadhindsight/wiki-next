28ae1f8106d0538f235dee83d95b709bb048d07f
chad hinds <chadrickj8@gmail.com>
2023-05-01 12:15:36 -0400

    Uninstall MUI

--- app/api/components/Header.tsx
+++ app/api/components/Header.tsx
@@ -1,6 +1,3 @@
-//NB: Because materialUI uses {useRef} under the hood, I had to make this a client component :(
-import { AppBar, Typography } from '@mui/material';
-
 const Header = () => {
     return (
         <>Header ting</>
