7a43e043afd79078c0f69ff3a9c3852a99917e7e
chad hinds <chadrickj8@gmail.com>
2023-05-01 11:27:39 -0400

    wiki endpoint

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import Header from './api/components/Header'
 
 
 export default function Home() {
+  // placeholder
+  // const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${searchQuery}`
 
   return (
     <>
