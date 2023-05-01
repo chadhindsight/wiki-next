a061378ce5e8721025f1916c203f9bd242e52000
chad hinds <chadrickj8@gmail.com>
2023-05-01 11:51:55 -0400

    Place the header is above main section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Header from './api/components/Header'
 
 export default function Home() {
 
   return (
     <>
-      <p>dfsdfs</p>
+      <Header />
+      <main className={styles.main}>
+      </main>
     </>
   )
 }
