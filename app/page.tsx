3d5cc17874b993a4e3ef4f586de86dc5aff75597
chad hinds <chadrickj8@gmail.com>
2023-05-03 11:29:38 -0400

    use Search component

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import styles from './page.module.css'
 import Header from './api/components/Header'
 import Footer from './api/components/Footer'
+import Search from './api/components/Search'
 
 export default function Home() {
 
@@ -9,6 +10,7 @@ export default function Home() {
     <>
       <Header />
       <main className={styles.main}>
+        <Search />
         <Footer />
       </main>
     </>
