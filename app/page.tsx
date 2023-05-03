ae4b83a829ceb2d2de1cd9650c24f1f319e94a8b
chad hinds <chadrickj8@gmail.com>
2023-05-03 09:07:24 -0400

    Remove commented out code

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,6 @@ import Header from './api/components/Header'
 import Footer from './api/components/Footer'
 
 export default function Home() {
-  // placeholder
-  // const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${searchQuery}`
 
   return (
     <>
