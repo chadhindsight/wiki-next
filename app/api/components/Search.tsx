d586da2dccda3146fabcf09cf5f2a386c1e29f21
chad hinds <chadrickj8@gmail.com>
2023-05-03 11:30:17 -0400

    Make a Details inferface that specifies what result objects should looks like

--- app/api/components/Search.tsx
+++ app/api/components/Search.tsx
@@ -2,6 +2,15 @@
 import { FormEvent, useState } from "react";
 
 
+interface Details {
+    ns: number;
+    pageid: number;
+    size: number;
+    snippet: string;
+    timestamp: string
+    title: string
+    wordcount: string
+}
 
 const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=`;
 
@@ -9,7 +18,7 @@ const Search = () => {
     // State to track userInput
     const [query, setQuery] = useState<string>("");
     // State to store search results
-    const [results, setResults] = useState([])
+    const [results, setResults] = useState<Array<Details>>([])
 
 
     // Calls wiki enpoint on form submission
@@ -27,14 +36,30 @@ const Search = () => {
         setQuery("")
     }
 
-    console.log(results)
+    const displayResults = function () {
+        return results?.map(result => {
 
+            // console.log(Object.keys(result), 'dsfsd')
+            console.log(result)
+            return (
 
+                <a ng-href="https://en.wikipedia.org/?curid=6678" target="_blank" ng-repeat="result in results" class="ng-scope" href="https://en.wikipedia.org/?curid=6678">
+                    <li>
+                        <h1 className="ng-binding">Cat</h1>
+                        <p className="ng-binding">The cat (Felis catus) is a domestic species of small carnivorous mammal.</p>
+                    </li>
+                </a>
+            )
+
+        })
+    }
+    displayResults()
     return (
         <>
             <form onSubmit={handleSubmit}>
                 <input value={query} onChange={(e) => setQuery(e.target.value)} />
             </form>
+            <ul>{ }</ul>
         </>
     );
 };
