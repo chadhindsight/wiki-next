cf3801c8155e99e01e2eb0a4a664da4c32838f24
chad hinds <chadrickj8@gmail.com>
2023-05-03 09:18:31 -0400

    set query state initial value, and use typechecking

--- app/api/components/Search.tsx
+++ app/api/components/Search.tsx
@@ -1,6 +1,14 @@
 "use client";
 
+import { useState } from "react";
+
+
 const Search = () => {
+    // State to track userInput
+    const [query, setQuery] = useState<String>('')
+
+
+    const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${searchQuery}`
 
     return (
         <div>
