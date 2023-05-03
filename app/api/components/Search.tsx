a20bf5edb1ad36d1d07c57917092ddb906af28f7
chad hinds <chadrickj8@gmail.com>
2023-05-03 10:36:49 -0400

    Change var name to apiSearchResults so it's more clear

--- app/api/components/Search.tsx
+++ app/api/components/Search.tsx
@@ -1,19 +1,41 @@
 "use client";
+import { FormEvent, useState } from "react";
 
-import { useState } from "react";
 
+
+const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=`;
 
 const Search = () => {
     // State to track userInput
-    const [query, setQuery] = useState<String>('')
+    const [query, setQuery] = useState<string>("");
+    // State to store search results
+    const [results, setResults] = useState([])
 
 
-    const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${searchQuery}`
+    // Calls wiki enpoint on form submission
+    const handleSubmit = async (e: FormEvent) => {
+        e.preventDefault()
+
+        const response = await fetch(`${endpoint}${query}`)
+        const apiSearchResults = await response.json()
+
+        // Destructures the list of results returns from api
+        const { search } = apiSearchResults.query
+
+        setResults(search)
+        // Clears input field when form gets submitted
+        setQuery("")
+    }
+
+    console.log(results)
+
 
     return (
-        <div>
-
-        </div>
+        <>
+            <form onSubmit={handleSubmit}>
+                <input value={query} onChange={(e) => setQuery(e.target.value)} />
+            </form>
+        </>
     );
 };
 
