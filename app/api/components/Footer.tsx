5041e0a7a88b465a5ad869dd57fb75e3351489ca
chad hinds <chadrickj8@gmail.com>
2023-05-04 12:54:05 -0400

    Increase footer width, change name of the header class

--- app/api/components/Footer.tsx
+++ app/api/components/Footer.tsx
@@ -2,7 +2,7 @@
 const Footer = () => {
     return (
         <footer
-            className="bg-neutral-200 text-center dark:bg-neutral-700 lg:text-left">
+            className="bg-neutral-200 text-center dark:bg-neutral-700 lg:text-left footer">
             <div className="p-4 text-center text-neutral-700 dark:text-neutral-200">
                 © {new Date().getFullYear()} Copyright:
                 <a
